1013227f120757f6251c9138d1eb00f94d9ba8a4
Jelena-J05 <135748656+Jelena-J05@users.noreply.github.com>
2023-06-22 16:35:30 +0200

    Add files via upload

---
+++ esercizio M2-D1.js
@@ -0,0 +1,9 @@
+/* Esercizio 1- M2
+1. Cos'è un algoritmo?
+Algoritmo è una serie di azioni che consentono ad un'operazione sui dati di funzionare bene durante ogni esecuzione.
+2. Cos'è una variabile?
+Una variabile rappresenta un contenitore nel quale possiamo fare uno storage dei risultati parziali.
+3. Undefined e null sono la stessa cosa?
+Undefined e null non sono la stessa cosa. Undefined significa che la variabile è stata dichiarata, ma il suo valore non è stato assegnato. 
+Null indica un valore vuoto.*/
+
